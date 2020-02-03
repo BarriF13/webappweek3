21c2935e08f08b3058a735e5a79cc3959ede32f8
BarriF13 <info@bdesignstudio.co.uk>
2020-02-03 18:30:49 +0000

    params

--- app.rb
+++ app.rb
@@ -9,8 +9,14 @@ end
 get '/another' do
   "this is another route"
 end
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb :index
-  
+end
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @color = params[:color]
+  @adorable = params[:adorable]
+  erb :index
 end
